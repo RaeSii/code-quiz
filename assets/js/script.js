9fda0eac057ae3caf3a6c7d8889986912ec93ff1
raesii <leisolo@gmail.com>
2022-01-22 14:39:37 -0800

    timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,29 @@
 var startButton = document.getElementById('start-btn');
 var questionEl = document.getElementById('question-container');
-var timerId = document.getElementById('time');
-
-
+var newQuestion = document.getElementById('question');
+var answerButton1 = document.getElementById('answer1');
+var answerButton2 = document.getElementById('answer2');
+var answerButton3 = document.getElementById('answer3');
+var answerButton4 = document.getElementById('answer4');
+var timerEl = document.getElementById('timer');
+var timerID;
+var time = 45;
+var currentQuestion = 0
 function startQuiz() {
     var startScreenEl = document.getElementById('start-screen');
     startScreenEl.setAttribute("class", "hide");
     questionEl.setAttribute("style", "display:block");
-    timerId = setInterval(clockTick, 500);
-    timerEl.textContent = time;
+    timerID = setInterval(clockTick, 500);
+    
     nextQuestion();
 }
-
-function nextQuestion() {
-
+function clockTick(){
+    timerEl.textContent = "Time Left: "+time;
+    if (time > 0) {
+        time--
+    } else{
+        endQuiz()
+    }
 }
 
 function selectAnswer() {
@@ -21,7 +31,7 @@ function selectAnswer() {
 }
 startButton.addEventListener('click', startQuiz)
 
-var question = [
+var questionBank = [
     {
         question: "1Commonly used data DO NOT include:",
         answers: [
@@ -60,3 +70,13 @@ var question = [
         correct: 2
     }
 ];
+function endQuiz(){
+    clearInterval (timerID)
+}
+function nextQuestion() {
+    newQuestion.textContent = questionBank [currentQuestion].question
+    answerButton1.textContent = questionBank [currentQuestion].answers[0]
+    answerButton2.textContent = questionBank [currentQuestion].answers[1]
+    answerButton3.textContent = questionBank [currentQuestion].answers[2]
+    answerButton4.textContent = questionBank [currentQuestion].answers[3]
+    }
