f5d0e14eaaf5bc8095e01035536267d24daf1e52
raesii <leisolo@gmail.com>
2022-01-22 14:52:55 -0800

    next-question

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,29 +5,48 @@ var answerButton1 = document.getElementById('answer1');
 var answerButton2 = document.getElementById('answer2');
 var answerButton3 = document.getElementById('answer3');
 var answerButton4 = document.getElementById('answer4');
+var results = document.getElementById('results')
 var timerEl = document.getElementById('timer');
 var timerID;
 var time = 45;
 var currentQuestion = 0
+answerButton1.addEventListener('click', selectAnswer)
+answerButton2.addEventListener('click', selectAnswer)
+answerButton3.addEventListener('click', selectAnswer)
+answerButton4.addEventListener('click', selectAnswer)
 function startQuiz() {
     var startScreenEl = document.getElementById('start-screen');
     startScreenEl.setAttribute("class", "hide");
     questionEl.setAttribute("style", "display:block");
     timerID = setInterval(clockTick, 500);
-    
+
     nextQuestion();
 }
-function clockTick(){
-    timerEl.textContent = "Time Left: "+time;
+function clockTick() {
+    timerEl.textContent = "Time Left: " + time;
     if (time > 0) {
         time--
-    } else{
+    } else {
         endQuiz()
     }
 }
 
 function selectAnswer() {
-
+    var userAnswer = this.getAttribute('data-value')
+    console.log(userAnswer)
+    if (userAnswer == questionBank[currentQuestion].correct) {
+        results.textContent = 'You are right!'
+    } else {
+        time -= 5;
+        results.textContent = 'You are wrong!'
+    }
+    if (currentQuestion < questionBank.length - 1) {
+        currentQuestion++
+        nextQuestion()
+    }
+    else {
+        endQuiz()
+    }
 }
 startButton.addEventListener('click', startQuiz)
 
@@ -35,7 +54,7 @@ var questionBank = [
     {
         question: "1Commonly used data DO NOT include:",
         answers: [
-            'strings','booleans', 'alerts', 'numbers'
+            'strings', 'booleans', 'alerts', 'numbers'
         ],
         correct: 2
     },
@@ -43,7 +62,7 @@ var questionBank = [
     {
         question: "2Commonly used data DO NOT include:",
         answers: [
-            'strings','booleans', 'alerts', 'numbers'
+            'strings', 'booleans', 'alerts', 'numbers'
         ],
         correct: 2
     },
@@ -51,32 +70,32 @@ var questionBank = [
     {
         question: "3Commonly used data DO NOT include:",
         answers: [
-            'strings','booleans', 'alerts', 'numbers'
+            'strings', 'booleans', 'alerts', 'numbers'
         ],
         correct: 2
     },
     {
         question: "4Commonly used data DO NOT include:",
         answers: [
-            'strings','booleans', 'alerts', 'numbers'
+            'strings', 'booleans', 'alerts', 'numbers'
         ],
         correct: 2
     },
     {
         question: "5Commonly used data DO NOT include:",
         answers: [
-            'strings','booleans', 'alerts', 'numbers'
+            'strings', 'booleans', 'alerts', 'numbers'
         ],
         correct: 2
     }
 ];
-function endQuiz(){
-    clearInterval (timerID)
+function endQuiz() {
+    clearInterval(timerID)
 }
 function nextQuestion() {
-    newQuestion.textContent = questionBank [currentQuestion].question
-    answerButton1.textContent = questionBank [currentQuestion].answers[0]
-    answerButton2.textContent = questionBank [currentQuestion].answers[1]
-    answerButton3.textContent = questionBank [currentQuestion].answers[2]
-    answerButton4.textContent = questionBank [currentQuestion].answers[3]
-    }
+    newQuestion.textContent = questionBank[currentQuestion].question
+    answerButton1.textContent = questionBank[currentQuestion].answers[0]
+    answerButton2.textContent = questionBank[currentQuestion].answers[1]
+    answerButton3.textContent = questionBank[currentQuestion].answers[2]
+    answerButton4.textContent = questionBank[currentQuestion].answers[3]
+}
